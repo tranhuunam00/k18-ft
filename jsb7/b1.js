d221d3067f5b7d2415ee1096df8e08ea7f839437
namth <tranhuunam23022000@gmail.com>
2023-09-18 21:32:25 +0700

    update

--- jsb7/b1.js
+++ jsb7/b1.js
@@ -25,14 +25,20 @@ const users = [
   { id: 3, age: 32 },
 ]; //0X99
 
-const users2 = []; // 0x100
+// 0x100
 
 // in key : 0,1,2,3
 // of
-for (let index in users) {
-  const newItem = { id: users[index].id, age: users[index].age };
-  users2.push(newItem);
-}
+// for (let index in users) {
+//   const newItem = { i d: users[index].id, age: users[index].age };
+//   users2.push(newItem);
+// }
+
+const users2 = users.map((value, index) => {
+  return value;
+});
+
+// map map():
 
 users.push({
   id: 4,
@@ -43,3 +49,33 @@ users[0].age = 90;
 
 console.log("users", users);
 console.log("users2", users2);
+
+const users3 = [
+  { id: 1, age: 12 },
+  { id: 2, age: 22 },
+  { id: 3, age: 32 },
+];
+const total = users3.reduce((prev, curr, index) => {
+  // vòng 1 giá trị prev = giá trị khởi đầu
+  // giá trị prev vòng sau = giá trị return vòng trước
+  console.log("vòng ", index);
+  console.log("prev", prev);
+  console.log("curr", curr);
+
+  return (
+    prev +
+    " user" +
+    (index + 1) +
+    " có id là " +
+    curr.id +
+    " và tuổi là " +
+    curr.age
+  );
+}, "");
+
+// user 1 có id là 1 và tuổi là 12, user 2 có id là 2 và tuổi là 14, user 3 có id là 3 và tuổi là 34
+console.log("total", total);
+console.log({} + "text");
+console.log(true + "text");
+console.log(1 + "text");
+console.log("text" + 2 + 1);
