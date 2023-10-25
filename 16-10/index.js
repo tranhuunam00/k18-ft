a1c3e644e3e1f8a7662695b64ddeee7d864536f1
unknown <quyet.vuong0@gmail.com>
2023-10-25 18:18:34 +0700

    save

--- 16-10/index.js
+++ 16-10/index.js
@@ -1,60 +1,50 @@
 const express = require('express')
 const qs = require('querystring')
 var bodyParser = require('body-parser')
-// const STATUS_CODE = require('./constants/httpResponseCode')
-// const HTTP_MESSAGE = require('./constants/httpErrorMessage')
-// const UTIL_HELPER = require('./helper/util')
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-const app = express()
-document.addEventListener('DOMContentLoaded', function () {
-    const container = document.getElementsByClassName('container')
-
-    const singup = document.getElementsByClassName('singup')
-    const signupInput = document.getElementById('signupInput')
-    const singupEmail = document.getElementById('singupEmail')
-    const singupPassword = document.getElementById('singupPassword')
-    const singupButton = document.getElementById('singupButton')
-
-    const login = document.getElementsByClassName('login')
-    const loginInput = document.getElementById('loginInput')
-    const loginEmail = document.getElementById('loginEmail')
-    const loginPassword = document.getElementById('loginPassword')
-    const loginButton = document.getElementById('loginButton')
-
-    const list = document.getElementsByClassName('list')
-    const listInput = document.getElementById('listInput')
-    const listEmail = document.getElementById('listEmail')
-    const listPassword = document.getElementById('listPassword')
-    const listButton = document.getElementById('listButton')
-
-    const fix = document.getElementsByClassName('fix')
-    const fixEmail = document.getElementById('fixEmail')
-    const fixPassword = document.getElementById('fixPassword')
-    const fixButton = document.getElementById('fixButton')
-})
-
-const randomUsersInit = (number) => {
-    const users = []
-    for (let i = 1; i <= number; i++) {
-        //todo
-        users.push({
-            id: new Date(),
-            ['name']: `name ${i}`,
-            age: i + 19,
-            password: '123456',
-            email: `${i}@gmail.com`,
-        })
-    }
-    return users
-}
 var path = require('path')
-
+const app = express()
+const loginButton = document.getElementById('loginButton')
 app.get('/', function (req, res) {
     var indexPath = path.join(__dirname, 'index.html')
     res.sendFile(indexPath)
-})
+    document
+        .getElementById('singupButton')
+        .addEventListener('click', function () {
+            var email = document.getElementById('singupEmail').value
+            var password = document.getElementById('singupPassword').value
 
+            if (email.length < 7) {
+                alert('Email phải có ít nhất 7 ký tự!')
+                return
+            }
+
+            if (password.length < 8) {
+                alert('Password phải có ít nhất 8 ký tự!')
+                return
+            }
+
+            // Tiến hành đăng ký nếu các điều kiện được đáp ứng
+            // Gửi dữ liệu đăng ký đến máy chủ (Node.js) bằng AJAX hoặc fetch API
+        })
+
+    document
+        .getElementById('loginButton')
+        .addEventListener('click', function () {
+            var email = document.getElementById('loginEmail').value
+            var password = document.getElementById('loginPassword').value
+
+            if (password.length < 8) {
+                alert('Password phải có ít nhất 8 ký tự!')
+                return
+            }
+
+            // Kiểm tra các điều kiện bổ sung như chữ cái viết hoa và số ở đây
+
+            // Tiến hành đăng nhập nếu các điều kiện được đáp ứng
+            // Gửi dữ liệu đăng nhập đến máy chủ (Node.js) bằng AJAX hoặc fetch API
+        })
+})
+app.get('/user', function (req, res) {})
 app.listen(3000, function () {
     console.log('Server is listening at http://localhost:3000')
 })
