f829f44c3d8f0b9c01981654438987069f0326d1
Nqdoanh167 <nqdcntt2002@gmail.com>
2023-11-06 21:03:58 +0700

    16/11

--- 16-10/util/googleAuth.js
+++ 16-10/util/googleAuth.js
@@ -1,19 +1,23 @@
-const { OAuth2Client } = require("google-auth-library");
+/** @format */
+
+const {OAuth2Client} = require('google-auth-library');
 
 const myOAuth2Client = new OAuth2Client(
-  "38529751211-d2tdls2dkji4h0hh0p30o3qto1h0rki7.apps.googleusercontent.com",
-  "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"
+   '890393797633-596ka17lnck19vrfm89219imacr206ji.apps.googleusercontent.com', //client id
+   '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', // client secret
+   'https://developers.google.com/oauthplayground',
 );
 
 myOAuth2Client.setCredentials({
-  refresh_token:
-    "1//04K2RLitflWXsCgYIARAAGAQSNwF-L9IruzDCZC_N2k7PDsyWsB4cnxXkublivcdTPl-cakuPoNGNuQnwhZKKKNGJHYe21UZj-FQ",
+   refresh_token:
+      '1//04pXdGE97YrqECgYIARAAGAQSNwF-L9IrxpY-HRrLWxPZSKFBqBZLrlYX-QUNmFTV7lowfHWSpf0hH1fvkSnEyXCfJ3fycdr9SEs',
 });
 
 const getAccessTokenGoogle = async () => {
-  const myAccessTokenObject = await myOAuth2Client.getAccessToken();
-  console.log("myAccessTokenObject",myAccessTokenObject?.token);
-  return myAccessTokenObject?.token;
+   const myAccessTokenObject = await myOAuth2Client.getAccessToken();
+   console.log('myAccessTokenObject', myAccessTokenObject?.token);
+   return myAccessTokenObject?.token;
 };
 
+getAccessTokenGoogle();
 module.exports = getAccessTokenGoogle;
