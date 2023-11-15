e140ce3a93f43be8ec24eb30dc6e732c22645579
unknown <quyet.vuong0@gmail.com>
2023-11-15 20:46:10 +0700

    delete

--- 16-10/index.js
+++ 16-10/index.js
@@ -8,31 +8,10 @@ const port = 3000
 app.use(bodyParser.json())
 
 const users = []
-
-app.post('/register', (req, res) => {
-    // Xử lý việc đăng ký người dùng ở đây
-    // ...
-
-    res.json({ message: 'Người dùng đã được đăng ký thành công' })
-})
-
-app.post('/login', (req, res) => {
-    // Xử lý việc đăng nhập ở đây
-    // ...
-
-    res.json({ message: 'Đăng nhập thành công' })
-})
-
 // Đường dẫn cho trang đăng nhập
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
-
-// Đường dẫn cho trang đăng ký
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'))
-})
-
 app.listen(port, () => {
     console.log(`Server đang lắng nghe trên cổng ${port}`)
 })
