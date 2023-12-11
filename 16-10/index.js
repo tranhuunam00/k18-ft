d14deee624d671a253e9c9c7c6a98553e6d7d70f
namTh <tranhuunam23022000@gmail.com>
2023-12-11 21:55:13 +0700

    update

--- 16-10/index.js
+++ 16-10/index.js
@@ -24,6 +24,41 @@ app.use("/order", orderRouter);
 
 connectMongo();
 
-app.listen(3000, function () {
+const server = app.listen(3000, function () {
   console.log("Server is listening at 3000");
+});
+
+const socket = require("socket.io");
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  },
+});
+require("./util/socketIo");
+io.on("connection", (socket) => {
+  console.log("New socket connection");
+
+  // Listen for incoming messages
+  socket.on("message", (message) => {
+    console.log(`Received message`, message);
+    // nhận từ ông A -> gửi tới ông B > nội dung > thời gina
+
+    // Broadcast the message to all connected sockets
+
+    // giả sử có ông B C D A F connected
+    // gửi cho tất cả A B C D E
+    // client: lọc xem message nào gửi cho mình dựa vào userId
+    // mình là ông b thì minh so sánh id của mình vs id của người nhận
+    // nếu == thì là nó gửi đến mình
+
+    io.emit("message", message);
+  });
+
+  // Disconnect event
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected");
+  });
 });
